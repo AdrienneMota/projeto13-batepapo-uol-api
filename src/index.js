c450ef34a3ae7862efdc9800541e0f16eb6450fd
AdrienneMota <adriennemota97@gmail.com>
2022-11-10 15:44:18 -0400

    post participant first version

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import express from "express" //usar protocolo http
+import cors from "cors" //evitar erro de segurança
+import { MongoClient } from "mongodb" //comunicação com o mongodb
+import dotenv from "dotenv" //permite que a aplicação seja usada por o outros bancos
+
+/////criacao da api
+const app = express() //criacao da api
+
+/////configurações da api
+app.use(cors()) //usar a biblioteca cors que evita erro de segurança
+app.use(express.json())//permite que a app entendo objetos json
+dotenv.config()//permite usar o documento .env
+
+/////o mongo db recebe o caminho para achar o banco 
+const mongodb = new MongoClient(process.env.MONGO_URI)
+let db 
+let participants 
+
+/////ligação com o banco
+try {
+    await mongodb.connect() //espero a api se conectar com o banco através do caminho que foi passado ao mongodb
+    db = mongodb.db("dbBatepapouol")//dentro do endereço passado ache o banco tal
+    participants = db.collection("participants")//dentro do banco tal ache a coleção tal
+} catch (error) {
+    console.log(error)
+}
+
+/////cadastro de participante
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+
+    if(!name){
+        res.status(422).send({message: "O nome do parcipante deve ser strings não vazio"})
+        return
+    }
+
+    const participant = {name, lastStatus: Date.now()}   
+
+    try {
+        await participants.insert(participant)
+        res.status(201).send({message: "Participante criado com sucesso."})
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.listen(5000, () => console.log(`Server is running in port: ${5000}`))
+
+
+

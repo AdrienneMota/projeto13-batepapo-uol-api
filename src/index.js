43de6e59768871edfef6aa2e4ac17d0b73bcde95
AdrienneMota <adriennemota97@gmail.com>
2022-11-10 20:52:11 -0400

    refactor: post

--- src/index.js
+++ src/index.js
@@ -2,56 +2,64 @@ import express from "express" //usar protocolo http
 import cors from "cors" //evitar erro de segurança
 import { MongoClient } from "mongodb" //comunicação com o mongodb
 import dotenv from "dotenv" //permite que a aplicação seja usada por o outros bancos
+import joi from "joi"
+import dayjs from "dayjs"
 
 /////criacao da api
 const app = express() //criacao da api
+
+//////confis dos objetos
+const participantSchema = joi.object({
+    name: joi.string().required().min(1)
+}) 
 
 /////configurações da api
 app.use(cors()) //usar a biblioteca cors que evita erro de segurança
 app.use(express.json())//permite que a app entendo objetos json
 dotenv.config()//permite usar o documento .env
+//const dayjs = require('dayjs')
 
 /////o mongo db recebe o caminho para achar o banco 
 const mongodb = new MongoClient(process.env.MONGO_URI)
 let db 
 let participants 
+let messages
 
 /////ligação com o banco
 try {
     await mongodb.connect() //espero a api se conectar com o banco através do caminho que foi passado ao mongodb
     db = mongodb.db("dbBatepapouol")//dentro do endereço passado ache o banco tal
     participants = db.collection("participants")//dentro do banco tal ache a coleção tal
+    messages = db.collection("messages")
 } catch (error) {
     console.log(error)
 }
 
 /////cadastro de participante
 app.post("/participants", async (req, res) => {
-    const { name } = req.body
-    let listParticipants
+    const candidateParticipant = req.body
+
+    //verificações
     
-    try {
-        listParticipants = await participants.find().toArray()
-    } catch (error) {
-        console.log(error)
+    const validation = participantSchema.validate(candidateParticipant, {abortEarly: false})
+    if(validation.error){
+        const erros = validation.error.details.map( d => d.message)
+        res.status(422).send(erros)
+        return
     }
-
-    const participantExist = listParticipants.find((p) => p.name === name)
+    
+    const participantExist = await participants.findOne({name: candidateParticipant.name})
     if(participantExist){
-        res.status(409).send({ message: "participante já existe, escolha outro nome"})
+        res.status(409).send({message: "participante já existe"})
         return
     }
-
-    if(!name){
-        res.status(422).send({message: "O nome do parcipante deve ser strings não vazio"})
-        return
-    }
-
-    const participant = {name, lastStatus: Date.now()}   
+    
+    //inserir participante   
 
     try {
-        await participants.insertOne(participant)
-        res.status(201).send({message: "Participante criado com sucesso."})
+        await participants.insertOne({name: candidateParticipant.name, lastStatus: Date.now()})
+        await messages.insertOne({from: candidateParticipant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: "now"})
+        res.status(201).send({message: "Participante cadastrado com sucesso."})
     } catch (error) {
         res.status(500).send(error)
     }
@@ -59,11 +67,22 @@ app.post("/participants", async (req, res) => {
 
 /////listagem de participantes
 app.get("/participants", async (req, res) => {
-    let participantsNow = []
 
     try {
-        participantsNow = await participants.find().toArray()
+        const participantsNow = await participants.find().toArray()
         res.send(participantsNow)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+/////listagem de menssagens
+app.get("/messages", async (req, res) => {
+
+    try {
+        const messagesNow = await messages.find().toArray()
+        res.send(messagesNow)
     } catch (error) {
         console.log(error)
         res.sendStatus(500)
